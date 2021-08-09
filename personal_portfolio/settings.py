d219fbcd480329f7113551e11e197bc0b81755c9
ralphiecodes <ironware6@gmail.com>
2021-08-09 04:21:44 -0400

    Local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -137,3 +137,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+try:
+    from .local_settings import *
+except ImportError:
+    print(" Looks like no local file. You must be on production")
