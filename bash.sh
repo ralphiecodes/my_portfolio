f9236877250eea654db0d7c2f201d7bff02d9a33
ralphiecodes <ironware6@gmail.com>
2021-08-25 19:37:14 -0400

    testing new bash commands

--- bash.sh
+++ bash.sh
@@ -1,10 +1,25 @@
 #! /bin/bash
-echo "What would you like your commit message to be?"
-read message
-echo  $(git add .)
-echo  $(git commit -m "$message ") 
-echo  $(git push origin main) 
+echo "What do you want to do?"
+read command
 
+
+if [[ $command == "branch" ]]; then
+
+    echo "which branch do you want to work on?"
+    read branch
+    echo $(git checkout $branch)
+
+elif [[ $command == "commit" ]]; then
+
+    echo "What would you like your commit message to be?"
+    read message
+    echo  $(git add .)
+    echo  $(git commit -m "$message ") 
+    echo  $(git push origin $branch) 
+
+else
+    echo "That's not a valid command"
+fi
 
 
 
